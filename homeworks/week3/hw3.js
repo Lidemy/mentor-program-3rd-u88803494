e484c29e4b72d01048e3bbb8179e2bc398ee2554
Hugh <u88803494@gmail.com>
2019-05-10 20:34:52 +0800

    'week3完成'

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,6 +1,7 @@
 function isPrime(n) {
   if (n === 1) return false;
-  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+  const primeTestTimes = Math.sqrt(n);
+  for (let i = 2; i <= primeTestTimes; i += 1) {
     if (n % i === 0) {
       return false;
     }
