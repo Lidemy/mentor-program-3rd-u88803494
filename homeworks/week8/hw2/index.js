ccc4eb8c509032942f496dcb42090617784c894e
Hugh <u88803494@gmail.com>
2019-06-23 19:13:23 +0800

    week8 完成了!

--- homeworks/week8/hw2/index.js
+++ homeworks/week8/hw2/index.js
@@ -6,17 +6,17 @@ function messageData() {
   request.send();
 } /* 試著做出送出資料後就把新留言同步上去，所以額外把發送命令打包。
 但發現這樣做其實有點難度，可能還要先清掉原本的資料，之後再把新資料弄上去。
-而且也發現會發生一些問題，在於資料上傳沒這麼快，所以一樣會遇到等待的問題，所以直接寫在送出的後面，
-會導致沒辦法撈到最新的資料。而且似乎會傳不上去？應該是因為有兩個 request 的關係吧？
-不過這部分感覺難度未明，而且現在人在公司不方便寫，就留給明天，先思考 hw3 怎麼寫 */
+而且也發現會發生一些問題，在於資料上傳沒這麼快，所以一樣會遇到等待的問題，所以直接寫在送出的後面，會導致沒辦法撈到最新的資料。
+而且似乎會傳不上去？應該是因為有兩個 request 的關係吧？
+這部分感覺難度未明 */
 
-function post(json) {
+function post(message) {
   const obj = {
     content: '',
   };
-  obj.content = json;
+  obj.content = message;
   const uploadDate = JSON.stringify(obj); // 轉換成 json 格式
-  request.open('post', 'https://lidemy-book-store.herokuapp.com/posts', true);
+  request.open('POST', 'https://lidemy-book-store.herokuapp.com/posts', true);
   request.setRequestHeader('Content-type', 'application/json'); // 必須要加上才可以，用意應該是要分辨資料是哪一種
   request.send(uploadDate);
 }
